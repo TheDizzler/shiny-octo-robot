f8775d34eab54b7f089b819160cebfe9668a0b52
Andrea <suteki_da_ne_0@yahoo.com>
2014-05-02 17:11:25 -0700

    added basic UI framework

--- MasterFlow/gen/vdindustries/masterflow/R.java
+++ MasterFlow/gen/vdindustries/masterflow/R.java
@@ -883,6 +883,15 @@ containing a value of this type.
  Minimum width of the search view text entry area. 
          */
         public static final int abc_search_view_text_min_width=0x7f08000c;
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f080011;
+        public static final int activity_vertical_margin=0x7f080012;
     }
     public static final class drawable {
         public static final int abc_ab_bottom_solid_dark_holo=0x7f020000;
@@ -986,15 +995,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
+        public static final int action_settings=0x7f05004b;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
+        public static final int checklists=0x7f050041;
         public static final int collapseActionView=0x7f05000d;
+        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editPassword=0x7f050047;
+        public static final int editProject=0x7f050049;
+        public static final int editUser=0x7f050045;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1004,15 +1019,21 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f050043;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int notes=0x7f050042;
+        public static final int password=0x7f050046;
+        public static final int photo=0x7f050040;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
+        public static final int project=0x7f050048;
         public static final int radio=0x7f05002f;
+        public static final int reports=0x7f05003f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1032,11 +1053,12 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int trade_detail=0x7f05003e;
-        public static final int trade_detail_container=0x7f05003c;
-        public static final int trade_list=0x7f05003d;
+        public static final int trade_detail=0x7f05004a;
+        public static final int trade_detail_container=0x7f05003d;
+        public static final int trade_list=0x7f05003e;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int username=0x7f050044;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1088,7 +1110,11 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_trade_detail=0x7f030017;
+        public static final int activity_categories=0x7f030017;
+        public static final int activity_checklist=0x7f030018;
+        public static final int activity_floors=0x7f030019;
+        public static final int activity_login=0x7f03001a;
+        public static final int activity_trade_detail=0x7f03001b;
         /** 
     Layout alias to replace the single-pane version of the layout with a
     two-pane version on Large screens.
@@ -1106,8 +1132,10 @@ containing a value of this type.
 
     
          */
-        public static final int activity_trade_list=0x7f030018;
-        public static final int activity_trade_twopane=0x7f030019;
+        public static final int activity_trade_list=0x7f03001c;
+        public static final int activity_trade_twopane=0x7f03001d;
+        public static final int fragment_categories=0x7f03001e;
+        public static final int fragment_checklist=0x7f03001f;
         /** 
     Layout alias to replace the single-pane version of the layout with a
     two-pane version on Large screens.
@@ -1116,11 +1144,19 @@ containing a value of this type.
     http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
     
          */
-        public static final int fragment_deficiency=0x7f03001e;
-        public static final int fragment_deficiency_grid=0x7f03001a;
-        public static final int fragment_deficiency_list=0x7f03001b;
-        public static final int fragment_trade_detail=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int fragment_deficiency=0x7f030026;
+        public static final int fragment_deficiency_grid=0x7f030020;
+        public static final int fragment_deficiency_list=0x7f030021;
+        public static final int fragment_floors=0x7f030022;
+        public static final int fragment_login=0x7f030023;
+        public static final int fragment_trade_detail=0x7f030024;
+        public static final int support_simple_spinner_dropdown_item=0x7f030025;
+    }
+    public static final class menu {
+        public static final int categories=0x7f0c0000;
+        public static final int checklist=0x7f0c0001;
+        public static final int floors=0x7f0c0002;
+        public static final int login=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1163,8 +1199,23 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_settings=0x7f0a0019;
         public static final int app_name=0x7f0a000d;
+        public static final int checklists=0x7f0a0016;
+        public static final int hello_world=0x7f0a001b;
+        public static final int login=0x7f0a0014;
+        public static final int notes=0x7f0a0015;
+        public static final int password=0x7f0a0012;
+        public static final int photo=0x7f0a0018;
+        public static final int project=0x7f0a0013;
+        public static final int reports=0x7f0a0017;
+        public static final int title=0x7f0a0010;
+        public static final int title_activity_categories=0x7f0a001a;
+        public static final int title_activity_checklist=0x7f0a001d;
+        public static final int title_activity_floors=0x7f0a001c;
+        public static final int title_activity_login=0x7f0a000f;
         public static final int title_trade_detail=0x7f0a000e;
+        public static final int username=0x7f0a0011;
     }
     public static final class style {
         /** 
