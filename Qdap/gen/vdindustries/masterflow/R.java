fc4fb00dde252d077e85300e8a5b75793e5d341c
Cholisky <Cholisky@gmail.com>
2014-05-08 16:18:16 -0700

    Minor changes to the previously modified layout files small changes to the checklists fragment removed title from the login fragment

--- Qdap/gen/vdindustries/masterflow/R.java
+++ Qdap/gen/vdindustries/masterflow/R.java
@@ -1146,7 +1146,8 @@ containing a value of this type.
         public static final int fragment_floors=0x7f030022;
         public static final int fragment_login=0x7f030023;
         public static final int fragment_trade_detail=0x7f030024;
-        public static final int support_simple_spinner_dropdown_item=0x7f030025;
+        public static final int report_item_layout=0x7f030025;
+        public static final int support_simple_spinner_dropdown_item=0x7f030026;
     }
     public static final class menu {
         public static final int categories=0x7f0c0000;
