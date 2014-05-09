6eda13bdea847e8d98b07e4d276d52f6ce9bf16a
ac <andrea.ching08@gmail.com>
2014-05-08 17:37:00 -0700

    child below expandable list view in progress, added methods for deficiency parser

--- Qdap/src/qdap/Floors.java
+++ Qdap/src/qdap/Floors.java
@@ -129,32 +129,17 @@ public class Floors extends Activity {
 		listDataHeader.addAll(DeficiencyParser.getFloors());
 
 		// Adding child data
-		List<String> top250 = new ArrayList<String>();
-		top250.add("The Shawshank Redemption");
-		top250.add("The Godfather");
-		top250.add("The Godfather: Part II");
-		top250.add("Pulp Fiction");
-		top250.add("The Good, the Bad and the Ugly");
-		top250.add("The Dark Knight");
-		top250.add("12 Angry Men");
+		List<String> floor2 = new ArrayList<String>();
+		floor2.addAll(DeficiencyParser.getRooms("Floor 2"));
 
-		List<String> nowShowing = new ArrayList<String>();
-		nowShowing.add("The Conjuring");
-		nowShowing.add("Despicable Me 2");
-		nowShowing.add("Turbo");
-		nowShowing.add("Grown Ups 2");
-		nowShowing.add("Red 2");
-		nowShowing.add("The Wolverine");
+		List<String> floor3 = new ArrayList<String>();
+		floor3.addAll(DeficiencyParser.getRooms("Floor 3"));
 
-		List<String> comingSoon = new ArrayList<String>();
-		comingSoon.add("2 Guns");
-		comingSoon.add("The Smurfs 2");
-		comingSoon.add("The Spectacular Now");
-		comingSoon.add("The Canyons");
-		comingSoon.add("Europa Report");
+		List<String> ph = new ArrayList<String>();
+		ph.addAll(DeficiencyParser.getRooms("PH"));
 
-		listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-		listDataChild.put(listDataHeader.get(1), nowShowing);
-		listDataChild.put(listDataHeader.get(2), comingSoon);
+		listDataChild.put(listDataHeader.get(0), floor2); // Header, Child data
+		listDataChild.put(listDataHeader.get(1), floor3);
+		listDataChild.put(listDataHeader.get(2), ph);
 	}
 }
--- Qdap/src/vdindustries/masterflow/content/DeficiencyParser.java
+++ Qdap/src/vdindustries/masterflow/content/DeficiencyParser.java
@@ -20,168 +20,170 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
-
-
 public class DeficiencyParser {
-	
-	public static InputStream	projectXML;
-	public static Element		root;
-	public static NodeList		listFloorNodes;
-	public static NodeList		listRoomNodes;
-	
-	public static NodeList		listTrades;
-	private static AssetManager		assMan;
-	
-	
-	
+
+	public static InputStream projectXML;
+	public static Element root;
+	public static NodeList listFloorNodes;
+	public static NodeList listRoomNodes;
+
+	public static NodeList listTrades;
+	private static AssetManager assMan;
+
 	public DeficiencyParser(AssetManager am) {
-	
+
 		assMan = am;
 		try {
 			projectXML = assMan.open("testproject.xml");
-			
-			Document xmlDoc = DocumentBuilderFactory.newInstance().
-				newDocumentBuilder().parse(projectXML);
+
+			Document xmlDoc = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder().parse(projectXML);
 			root = xmlDoc.getDocumentElement();
 			root.normalize();
 			listFloorNodes = root.getElementsByTagName("floor");
 			listTrades = root.getElementsByTagName("trade");
 			listRoomNodes = root.getElementsByTagName("room");
-			
+
 		} catch (SAXException | IOException | ParserConfigurationException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	/** Retrieves number of deficiencies in a room, completed & uncompleted */
 	public static int totalDefByUnit(String unit) {
-	
+
 		for (int i = 0; i < listRoomNodes.getLength(); ++i) {
-			
+
 			Element room = ((Element) listRoomNodes.item(i));
 			if (room.getAttribute(Deficiency.FLOORID).equalsIgnoreCase(unit)) {
-				
+
 				return room.getElementsByTagName("deficiency").getLength();
 			}
 		}
-		
+
 		return 0;
 	}
-	
-	
+
 	public static int outstandingDefByUnit(String unit) {
-	
-		
+
 		return 5;
 	}
-	
-	/** Retrieves a deficiency object from its reportID.
-	 * Note: this is likely very inefficient in the case of large buildings. */
+
+	/**
+	 * Retrieves a deficiency object from its reportID. Note: this is likely
+	 * very inefficient in the case of large buildings.
+	 */
 	public static Deficiency getDefByID(String reportID) {
-	
+
 		NodeList defList = root.getElementsByTagName("deficiency");
 		for (int i = 0; i < defList.getLength(); ++i) {
-			
-			if (((Element) defList.item(i)).
-				getAttribute(Deficiency.REPORTID).
-				equalsIgnoreCase(reportID)) {
-				
+
+			if (((Element) defList.item(i)).getAttribute(Deficiency.REPORTID)
+					.equalsIgnoreCase(reportID)) {
+
 				return parseDeficiency(defList.item(i));
 			}
 		}
 		return null;
 	}
-	
-	
+
 	/** Builds a list of deficiencies by trade. */
 	public static List<Deficiency> getDefByTradeList(String tradeSelected) {
-	
+
 		List<Deficiency> defList = new ArrayList<Deficiency>();
-		
+
 		// for each <trade> element
 		for (int i = 0; i < listTrades.getLength(); ++i) {
-			
+
 			// if <trade type = tradeSelected>
-			if (((Element) listTrades.item(i)).getAttribute("type").equalsIgnoreCase(tradeSelected)) {
-				
+			if (((Element) listTrades.item(i)).getAttribute("type")
+					.equalsIgnoreCase(tradeSelected)) {
+
 				// get a list of all child deficiency elements
-				NodeList nodes = ((Element) listTrades.item(i)).getElementsByTagName("deficiency");
-				
+				NodeList nodes = ((Element) listTrades.item(i))
+						.getElementsByTagName("deficiency");
+
 				for (int j = 0; j < nodes.getLength(); ++j)
 					defList.add(parseDeficiency(nodes.item(j)));
 			}
 		}
-		
+
 		return defList;
 	}
-	
-	
+
 	/** Retrieves room plan image file location from it's room number. */
 	private static String getRoomImageFile(String roomNo) {
-	
+
 		for (int i = 0; i < listRoomNodes.getLength(); ++i) {
-			
+
 			Element current = (Element) listRoomNodes.item(i);
 			if (current.getAttribute(Deficiency.ROOMNO).equals(roomNo)) {
-				
-				return ((Element) current.
-					getElementsByTagName(Deficiency.ROOMPLAN).item(0)).
-					getAttribute(Deficiency.ROOMIMAGE);
+
+				return ((Element) current.getElementsByTagName(
+						Deficiency.ROOMPLAN).item(0))
+						.getAttribute(Deficiency.ROOMIMAGE);
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/** Retrieves floor plan image file location from it's ID. */
 	private static String getFloorImageFile(String floorID) {
-	
-		
+
 		for (int i = 0; i < listFloorNodes.getLength(); ++i) {
-			
+
 			Element current = (Element) listFloorNodes.item(i);
 			if (current.getAttribute(Deficiency.FLOORID).equals(floorID)) {
-				
-				return ((Element) current.
-					getElementsByTagName(Deficiency.FLOORPLAN).item(0)).
-					getAttribute(Deficiency.FLOORIMAGE);
+
+				return ((Element) current.getElementsByTagName(
+						Deficiency.FLOORPLAN).item(0))
+						.getAttribute(Deficiency.FLOORIMAGE);
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	/** Builds a deficiency object from a XML node. */
 	private static Deficiency parseDeficiency(Node node) {
-	
+
 		Deficiency def = new Deficiency();
 		Element defElem = (Element) node;
-		
+
 		def.reportID = defElem.getAttribute(Deficiency.REPORTID);
-		
-		def.completed = Boolean.parseBoolean(defElem.getElementsByTagName(Deficiency.COMPLETED).item(
-			0).getTextContent());
-		def.priority = Boolean.parseBoolean(defElem.getElementsByTagName(Deficiency.PRIORITY).item(
-			0).getTextContent());
-		Element coor = ((Element) defElem.getElementsByTagName("coordinates").item(0));
+
+		def.completed = Boolean.parseBoolean(defElem
+				.getElementsByTagName(Deficiency.COMPLETED).item(0)
+				.getTextContent());
+		def.priority = Boolean.parseBoolean(defElem
+				.getElementsByTagName(Deficiency.PRIORITY).item(0)
+				.getTextContent());
+		Element coor = ((Element) defElem.getElementsByTagName("coordinates")
+				.item(0));
 		def.X = Integer.parseInt(coor.getAttribute(Deficiency.XCOORD));
 		def.Y = Integer.parseInt(coor.getAttribute(Deficiency.YCOORD));
-		
-		def.object = defElem.getElementsByTagName(Deficiency.OBJECT).item(0).getTextContent();
-		def.item = defElem.getElementsByTagName(Deficiency.ITEM).item(0).getTextContent();
-		def.verb = defElem.getElementsByTagName(Deficiency.VERB).item(0).getTextContent();
-		def.direction = defElem.getElementsByTagName(Deficiency.DIRECTION).item(0).getTextContent();
-		def.location = defElem.getElementsByTagName(Deficiency.LOCATION).item(0).getTextContent();
-		
-		def.trade = ((Element) defElem.getParentNode()).getAttribute(Deficiency.TRADE);
-		def.roomNo = ((Element) defElem.getParentNode().getParentNode()).getAttribute(Deficiency.ROOMNO);
+
+		def.object = defElem.getElementsByTagName(Deficiency.OBJECT).item(0)
+				.getTextContent();
+		def.item = defElem.getElementsByTagName(Deficiency.ITEM).item(0)
+				.getTextContent();
+		def.verb = defElem.getElementsByTagName(Deficiency.VERB).item(0)
+				.getTextContent();
+		def.direction = defElem.getElementsByTagName(Deficiency.DIRECTION)
+				.item(0).getTextContent();
+		def.location = defElem.getElementsByTagName(Deficiency.LOCATION)
+				.item(0).getTextContent();
+
+		def.trade = ((Element) defElem.getParentNode())
+				.getAttribute(Deficiency.TRADE);
+		def.roomNo = ((Element) defElem.getParentNode().getParentNode())
+				.getAttribute(Deficiency.ROOMNO);
 		return def;
 	}
-	
-	
+
 	public static void loadRoomPlan(ImageView image, String roomNo) {
-		
+
 		String file = getRoomImageFile(roomNo);
 		try {
 			image.setImageDrawable(loadImageFromAsset(file));
@@ -189,58 +191,92 @@ public class DeficiencyParser {
 			ex.printStackTrace();
 		}
 	}
-	
-	
-	
-	public static void loadFloorPlan(ImageView image, String floorID) {
+
+	// Getting a list of rooms
+	public static List<String> getRooms(String floorID) {
+		NodeList roomNodes = DeficiencyParser.listRoomNodes;
+		List<String> rooms = new ArrayList<String>();
+		for (int i = 0; i < listFloorNodes.getLength(); ++i) {
+
+			Element current = (Element) listFloorNodes.item(i);
+			if (current.getAttribute(Deficiency.FLOORID).equals(floorID)) {
+			roomNodes =	 current.getElementsByTagName(Deficiency.ROOM);
+			rooms.add(((Element) roomNodes.item(i))
+					.getAttribute("no"));
+					
+			}
 		
+	
+
+		}
+	
+		return rooms;
+	}
+
+	/** Retrieves a list of rooms on a specific floor. */
+	public static NodeList getRoomsByFloor(String floorID) {
+
+		for (int i = 0; i < listFloorNodes.getLength(); ++i) {
+
+			Element current = (Element) listFloorNodes.item(i);
+			if (current.getAttribute(Deficiency.FLOORID).equals(floorID)) {
+				return current.getElementsByTagName(Deficiency.ROOM);
+			}
+		}
+		return null;
+	}
+
+	public static void loadFloorPlan(ImageView image, String floorID) {
+
 		String file = getFloorImageFile(floorID);
 		try {
-			image.setImageDrawable(loadImageFromAsset(file)); //CHANGE THIS LINE
-//			image.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+			image.setImageDrawable(loadImageFromAsset(file)); // CHANGE THIS
+																// LINE
+			// image.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
-	
-	//Getting a list of floors
-	public static List<String> getFloors(){
+
+	// Getting a list of floors
+	public static List<String> getFloors() {
 		NodeList floorNodes = DeficiencyParser.listFloorNodes;
 		List<String> floors = new ArrayList<String>();
 		for (int i = 0; i < floorNodes.getLength(); ++i) {
 
-			String floor = ((Element) floorNodes.item(i)).getAttribute("floorID");
-			
+			String floor = ((Element) floorNodes.item(i))
+					.getAttribute("floorID");
+
 			floors.add(floor);
 		}
 		return floors;
 	}
-	
-	/** Load an image from a directory on device.
-	 * NOT YET IMPLEMENTED */
+
+	/**
+	 * Load an image from a directory on device. NOT YET IMPLEMENTED
+	 */
 	private Drawable loadImageFromDirectory(String file) {
-	
-		
-//				try {
-//				Bitmap bmap = BitmapFactory.decodeFile();
-//				} catch (IOException ex) {
-//					throw ex;
-//				}
+
+		// try {
+		// Bitmap bmap = BitmapFactory.decodeFile();
+		// } catch (IOException ex) {
+		// throw ex;
+		// }
 		return null;
-		
+
 	}
-	
-	/** Load an image from a URI.
-	 * NOT YET IMPLEMENTED */
+
+	/**
+	 * Load an image from a URI. NOT YET IMPLEMENTED
+	 */
 	private Drawable loadImageFromUri(String uri) {
-		
-		
+
 		return null;
 	}
-	
-	
+
 	/** Loads an image from the assets directory. */
-	protected static Drawable loadImageFromAsset(String file) throws IOException {
+	protected static Drawable loadImageFromAsset(String file)
+			throws IOException {
 		InputStream is = assMan.open(file);
 		return Drawable.createFromStream(is, null);
 	}
