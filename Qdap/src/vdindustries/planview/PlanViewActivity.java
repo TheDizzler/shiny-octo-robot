2ad6a18136c98ceacd7e9300bc07cae0d567745d
T-Dawg <tgillon.music@gmail.com>
2014-05-09 13:26:05 -0700

    Parser and Floors update

--- Qdap/src/vdindustries/planview/PlanViewActivity.java
+++ Qdap/src/vdindustries/planview/PlanViewActivity.java
@@ -1,7 +1,6 @@
 package vdindustries.planview;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
@@ -21,8 +20,8 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,16 +33,19 @@ import android.widget.LinearLayout;
 
 public class PlanViewActivity extends ActionBarActivity {
 	
-	List<Button>		buttons;
+	List<Button>				buttons;
 	/** Useful methods: resetZoom(), setZoom(float scale) */
-	ImageView			image;
-	Canvas				canvas;
-	List<Deficiency>	defs;
+	ImageView					image;
+	Canvas						canvas;
+	List<Deficiency>			defs;
 	
-	int					finalHeight, finalWidth;
+	int							finalHeight, finalWidth;
 	
-	Bitmap				plan;
-	private Context		context;
+	Bitmap						plan;
+	private Context				context;
+	private ArrayList<Button>	floorButtons;
+	private ArrayList<Button>	flagBtns;
+	
 	
 	@Override protected void onCreate(Bundle savedInstanceState) {
 	
@@ -63,13 +65,12 @@ public class PlanViewActivity extends ActionBarActivity {
 	private void showRoom(String room) {
 	
 		defs = new ArrayList<Deficiency>();
+		flagBtns = new ArrayList<Button>();
 		
-//		DeficiencyParser.loadRoomPlan(image, room);
 		Bitmap immutable = null;
 		String file = DeficiencyParser.getRoomImageFile(room);
 		try {
 			immutable = DeficiencyParser.loadBitmapFromAsset(file);
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -80,18 +81,31 @@ public class PlanViewActivity extends ActionBarActivity {
 		
 		for (int i = 0; i < defs.size(); ++i) {
 			
-			LinearLayout layout = new LinearLayout(context);
-			layout.setOrientation(LinearLayout.VERTICAL);
-			
-			layout.setLayoutParams(new LayoutParams(
-				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-			
-			Bitmap flag = BitmapFactory.decodeResource(getResources(),
-				R.drawable.flag_red);
-			
-			canvas.drawBitmap(flag, defs.get(i).X, defs.get(i).Y, null);
-//			layout.addView(flag);
-//			setContentView(layout);
+			Deficiency def = defs.get(i);
+			if (def.trade.equalsIgnoreCase("Framing")) {
+				LinearLayout layout = new LinearLayout(context);
+				layout.setOrientation(LinearLayout.VERTICAL);
+				
+				layout.setLayoutParams(new LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+				
+				Bitmap flag;
+				if (def.completed)
+					flag = BitmapFactory.decodeResource(getResources(),
+						R.drawable.flag_grey);
+				else if (def.priority)
+					flag = BitmapFactory.decodeResource(getResources(),
+						R.drawable.flag_red);
+				else
+					flag = BitmapFactory.decodeResource(getResources(),
+						R.drawable.flag_blue);
+				
+				
+				int x = defs.get(i).X;
+				int y = defs.get(i).Y;
+				canvas.drawBitmap(flag, x, y - flag.getHeight(), null);
+				createFlagButton(x, y);
+			}
 		}
 		
 //		BitmapDrawable dr = new BitmapDrawable(bmOverlay);
@@ -101,6 +115,28 @@ public class PlanViewActivity extends ActionBarActivity {
 		
 	}
 	
+	/** Creates an invisible button over a flag. */
+	private void createFlagButton(int x, int y) {
+	
+		Button btn = new Button(this);
+		btn.setVisibility(View.VISIBLE);
+		btn.setBackgroundColor(Color.TRANSPARENT);
+		btn.setX(x);
+		btn.setY(y);
+		
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override public void onClick(View v) {
+			
+				// DO STUFF
+			}
+		});
+		
+		flagBtns.add(btn);
+		
+	}
+	
+	
 	
 	/** Creates a button for each floor in XML file.
 	 * Must add android:id="@+id/linear_layout" to activity XML. */
@@ -109,7 +145,7 @@ public class PlanViewActivity extends ActionBarActivity {
 		LinearLayout l_layout = (LinearLayout) findViewById(R.id.linear_layout);
 		l_layout.setOrientation(LinearLayout.VERTICAL);
 		
-		buttons = new ArrayList<Button>();
+		floorButtons = new ArrayList<Button>();
 		NodeList floorNodes = DeficiencyParser.listFloorNodes;
 		
 		for (int i = 0; i < floorNodes.getLength(); ++i) {
